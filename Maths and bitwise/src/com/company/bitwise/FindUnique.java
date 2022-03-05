cf6161712f82a49869e0d168fde15eb837662dd6
KondaSidhartha <77008318+KondaSidhartha@users.noreply.github.com>
2022-03-05 22:50:38 +0530

    Update FindUnique.java

--- Maths and bitwise/src/com/company/bitwise/FindUnique.java
+++ Maths and bitwise/src/com/company/bitwise/FindUnique.java
@@ -4,6 +4,7 @@ public class FindUnique {
     public static void main(String[] args) {
         int[] arr = {2, 3, 3, 4, 2, 6, 4};
         System.out.println(ans(arr));
+        System.out.println("above is ans");
     }
 
     private static int ans(int[] arr) {
